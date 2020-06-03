3a0d798b2d5891086e1c405eee110adb9a321b96
Alex <ialexi.bl@gmail.com>
2020-06-03 21:37:46 +1000

    Changed file extensions

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,32 @@
-import terser from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
 
 module.exports = [
   {
     input: './src/grid.js',
     output: {
-      file: './grid.js',
+      file: './grid.cjs',
       format: 'cjs',
     },
   },
   {
     input: './src/picker.js',
     output: {
-      file: './picker.js',
+      file: './picker.cjs',
       format: 'cjs',
     },
   },
   {
     input: './src/numbers.js',
     output: {
-      file: './numbers.js',
+      file: './numbers.cjs',
       format: 'cjs',
     },
   },
   {
     input: './src/sequence.js',
     output: {
-      file: './sequence.js',
+      file: './sequence.cjs',
       format: 'cjs',
     },
   },
@@ -34,11 +34,11 @@ module.exports = [
     input: './src/index.js',
     output: [
       {
-        file: './dist/index.js',
+        file: './dist/index.cjs',
         format: 'cjs',
       },
       {
-        file: './dist/index.es.js',
+        file: './dist/index.mjs',
         format: 'es',
       },
     ],
